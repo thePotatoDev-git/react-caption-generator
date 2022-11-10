be21c5cab5e76e7a6927641c4558525835d27cc8
Albertus Chua <albertus.chua@gmail.com>
2022-11-10 17:13:59 -0500

    Added meme data

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import memesData from '../memesData';
 
 export default function Meme() {
+    function randomMeme() {
+        const memesArray = memesData.data.memes;
+        const randomNum = Math.floor(Math.random() * memesArray.length);
+        console.log(memesArray[randomNum].url);
+    }
+
     return (
         <section>
-            <form className="meme--form">
+            <div className="meme--form">
                 <input type="text" placeholder="Top text" className="meme--input"/>
                 <input type="text" placeholder="Bottom text" className="meme--input"/>
-                <button className="meme--button">Get a new meme image 🖼</button>
-            </form>
+                <button onClick={randomMeme} className="meme--button">Get a new meme image 🖼</button>
+            </div>
         </section>
     )
 }
