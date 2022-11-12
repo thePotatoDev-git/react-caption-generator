48610362fc528fac0fa649dada3882e590a17b6d
Albertus Chua <albertus.chua@gmail.com>
2022-11-12 17:01:25 -0500

    Refactored Meme component

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import memesData from '../memesData';
 
 export default function Meme() {
+    // const [memeImage, setMemeImage] = React.useState('');
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: '',
+    });
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
     function randomMeme() {
-        const memesArray = memesData.data.memes;
+        const memesArray = allMemeImages.data.memes;
         const randomNum = Math.floor(Math.random() * memesArray.length);
-        console.log(memesArray[randomNum].url);
+        const url = memesArray[randomNum].url;
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url,
+        }));
     }
 
     return (
@@ -14,6 +28,9 @@ export default function Meme() {
                 <input type="text" placeholder="Top text" className="meme--input"/>
                 <input type="text" placeholder="Bottom text" className="meme--input"/>
                 <button onClick={randomMeme} className="meme--button">Get a new meme image 🖼</button>
+            </div>
+            <div className="meme--image">
+                <img src={meme.randomImage} className="meme--image" />
             </div>
         </section>
     )
