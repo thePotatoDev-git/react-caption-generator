0fbd20d6589c04d57ea31ef32b1bb169fa845d4d
Albertus Chua <albertus.chua@gmail.com>
2022-11-18 18:57:51 -0500

    Added caption function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -22,15 +22,44 @@ export default function Meme() {
         }));
     }
 
+    function handleCaption(event) {
+        const {name, value} = event.target;
+        
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value,
+        }))
+    }
+
     return (
         <section>
             <div className="meme--form">
-                <input type="text" placeholder="Top text" className="meme--input"/>
-                <input type="text" placeholder="Bottom text" className="meme--input"/>
-                <button onClick={randomMeme} className="meme--button">Get a new meme image 🖼</button>
+                <input className="meme--input"
+                     type="text" 
+                     placeholder="Top text" 
+                     name="topText"
+                     value={meme.topText}
+                     onChange={handleCaption}
+                />
+                <input 
+                    type="text" 
+                    placeholder="Bottom text" 
+                    className="meme--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleCaption}
+                />
+                <button 
+                    onClick={randomMeme} 
+                    className="meme--button"
+                >
+                    Get a new meme image 🖼
+                </button>
             </div>
-            <div className="meme--image">
+            <div className="meme">
                 <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </section>
     )
